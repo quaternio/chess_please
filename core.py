bcc3e6753a06576ef1aef7e9c738b6bc0769287f
Thomas Noel <thomas.c.noel@gmail.com>
2022-10-22 12:45:33 -0700

    Added initial chess files

---
+++ core.py
@@ -0,0 +1,37 @@
+from assets import ChessBoard, Player
+import time
+
+def flirt_init_sequence():
+    p1_name = input("Player 1, what's your name?\n")
+    print("Wow...")
+    time.sleep(1)
+    print("That's a weird name...\n")
+    time.sleep(0.75)
+    p2_name = input("Player 2, what' your name?\n")
+    print("Now that on the other hand...")
+    time.sleep(1)
+    print("is a beautiful name!\n")
+    time.sleep(0.75)
+
+    return p1_name, p2_name
+
+def init_sequence():
+    p1_name = input("Player 1, what's your name?\n")
+    p2_name = input("Player 2, what' your name?\n")
+    return p1_name, p2_name
+
+def main():
+    chessboard = ChessBoard()
+    p1_name, p2_name = init_sequence()
+    p1 = Player(p1_name)
+    p2 = Player(p2_name)
+
+    session_active = True
+    while session_active:
+        chessboard.render_board()
+        p1_move = p1.specify_move()
+        move_valid = chessboard.make_move(p1_move, 1) 
+
+
+if __name__ == "__main__":
+    main()
---
+++ unicode_pieces.py
@@ -0,0 +1,22 @@
+ELEMENTS = {
+    'w': {
+        #'checker': u'\u25FB',
+        'checker': '_',
+        'pawn': u'\u265F',
+        'rook': u'\u265C',
+        'knight': u'\u265E',
+        'bishop': u'\u265D',
+        'king': u'\u265A',
+        'queen': u'\u265B',
+    },
+    'b': {
+        #'checker': u'\u25FC',
+        'checker': '_',
+        'pawn': u'\u2659',
+        'rook': u'\u2656',
+        'knight': u'\u2658',
+        'bishop': u'\u2657',
+        'king': u'\u2654',
+        'queen': u'\u2655'
+    }
+}
